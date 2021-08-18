7c2f7afb3da18c58b03a74984b9965d84cca9e11
Hanjun Kim <hallazzang@gmail.com>
2021-08-18 15:42:20 +0900

    add read-genesis command

--- cmd.go
+++ cmd.go
@@ -7,7 +7,11 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	genutiltypes "github.com/cosmos/cosmos-sdk/x/genutil/types"
 	liquiditytypes "github.com/gravity-devs/liquidity/x/liquidity/types"
 	"github.com/schollz/progressbar/v3"
 	"github.com/spf13/cobra"
@@ -20,7 +24,7 @@ func RootCmd() *cobra.Command {
 	}
 	cmd.AddCommand(
 		SummaryCmd(),
-		//ExtractCmd(),
+		ReadGenesisCmd(),
 	)
 	return cmd
 }
@@ -199,107 +203,62 @@ func SummaryCmd() *cobra.Command {
 	return cmd
 }
 
-//func ExtractCmd() *cobra.Command {
-//	var beginHeight, endHeight int64
-//	var outFileName string
-//	cmd := &cobra.Command{
-//		Use: "extract",
-//		RunE: func(cmd *cobra.Command, args []string) error {
-//			cmd.SilenceUsage = true
-//
-//			cfg, err := ReadClientConfig("config.toml")
-//			if err != nil {
-//				return fmt.Errorf("read client config: %w", err)
-//			}
-//
-//			c, err := NewClient(cfg)
-//			if err != nil {
-//				return fmt.Errorf("new client: %w", err)
-//			}
-//			defer c.Close()
-//
-//			ctx := context.Background()
-//
-//			if endHeight == 0 {
-//				latestBlockHeight, err := c.LatestBlockHeight(ctx)
-//				if err != nil {
-//					return fmt.Errorf("get latest block height: %w", err)
-//				}
-//				endHeight = latestBlockHeight
-//			}
-//
-//			if beginHeight == 0 {
-//				beginHeight = endHeight - 999
-//				if beginHeight < 1 {
-//					beginHeight = 1
-//				}
-//			}
-//
-//			if beginHeight > endHeight {
-//				return fmt.Errorf("begin height must be less or equal than end height")
-//			}
-//
-//			fmt.Printf("begin height = %d, end height = %d\n", beginHeight, endHeight)
-//
-//			query := fmt.Sprintf(`swap_transacted.pool_id EXISTS AND block.height >= %d AND block.height <= %d`, beginHeight, endHeight)
-//			heights, err := c.SearchBlockHeights(ctx, query)
-//			if err != nil {
-//				return fmt.Errorf("search block heights: %w", err)
-//			}
-//			if len(heights) == 0 {
-//				fmt.Println("no swap events found")
-//				return nil
-//			}
-//			fmt.Printf("searched heights (total %d) = %v\n", len(heights), heights)
-//
-//			fmt.Print("continue? [y/N] ")
-//			var ans string
-//			if _, err := fmt.Scanln(&ans); err != nil {
-//				return fmt.Errorf("read input: %w", err)
-//			}
-//			if strings.ToLower(ans) != "y" {
-//				return fmt.Errorf("aborted")
-//			}
-//
-//			outFile, err := os.OpenFile(outFileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-//			if err != nil {
-//				return fmt.Errorf("create out file: %w", err)
-//			}
-//			defer outFile.Close()
-//
-//			fmt.Printf("writing output to %s\n", outFile.Name())
-//
-//			bar := progressbar.Default(int64(len(heights)))
-//
-//			for _, height := range heights {
-//				events, err := c.EndBlockEvents(ctx, height)
-//				if err != nil {
-//					return fmt.Errorf("get end block events: %w", err)
-//				}
-//				block := Block{Height: height, Events: []interface{}{}}
-//				for _, event := range events {
-//					if event.Type == liquiditytypes.EventTypeSwapTransacted {
-//						ste, err := NewSwapTransactedEvent(event)
-//						if err != nil {
-//							return fmt.Errorf("new swap_transacted event: %w", err)
-//						}
-//						block.Events = append(block.Events, ste)
-//					}
-//				}
-//				if err := json.NewEncoder(outFile).Encode(block); err != nil {
-//					return fmt.Errorf("write event to stdout: %w", err)
-//				}
-//				if err := outFile.Sync(); err != nil {
-//					return fmt.Errorf("sync out file: %w", err)
-//				}
-//				_ = bar.Add(1)
-//			}
-//
-//			return nil
-//		},
-//	}
-//	cmd.Flags().Int64VarP(&beginHeight, "begin", "b", 0, "begin height")
-//	cmd.Flags().Int64VarP(&endHeight, "end", "e", 0, "end height")
-//	cmd.Flags().StringVarP(&outFileName, "outfile", "o", "output.log", "output file name")
-//	return cmd
-//}
+func ReadGenesisCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "read-genesis [file]",
+		Args:  cobra.ExactArgs(1),
+		Short: "Read genesis file and extract summary",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cmd.SilenceUsage = true
+
+			cfg, err := ReadClientConfig("config.toml")
+			if err != nil {
+				return fmt.Errorf("read client config: %w", err)
+			}
+
+			c, err := NewClient(cfg)
+			if err != nil {
+				return fmt.Errorf("new client: %w", err)
+			}
+			defer c.Close()
+
+			ctx := context.Background()
+
+			pools, err := c.Pools(ctx)
+			if err != nil {
+				return fmt.Errorf("get pools: %w", err)
+			}
+
+			poolCoinDenomSet := make(map[string]uint64) // (pool coin denom) => (pool id)
+			for _, pool := range pools {
+				poolCoinDenomSet[pool.PoolCoinDenom] = pool.Id
+			}
+
+			appState, _, err := genutiltypes.GenesisStateFromGenFile(args[0])
+			if err != nil {
+				return fmt.Errorf("genesis state from file: %w", err)
+			}
+
+			genState := banktypes.GetGenesisStateFromAppState(codec.NewProtoCodec(codectypes.NewInterfaceRegistry()), appState)
+
+			numPoolInvestors := make(map[uint64]int) // (pool id) => (num pool investors)
+
+			for _, balance := range genState.Balances {
+				for poolCoinDenom, poolID := range poolCoinDenomSet {
+					if balance.Coins.AmountOf(poolCoinDenom).IsPositive() {
+						numPoolInvestors[poolID]++
+					}
+				}
+			}
+
+			fmt.Println("number of pool investors")
+			fmt.Println("========================")
+			for _, pool := range pools {
+				fmt.Printf("pool %d: %d\n", pool.Id, numPoolInvestors[pool.Id])
+			}
+
+			return nil
+		},
+	}
+	return cmd
+}
